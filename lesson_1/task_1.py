4f337d4b729db4ea82c7a83c18a69dfcd00fb8bb
gbmikhail <vas1024@mail.ru>
2021-09-21 00:09:30 +0300

    Урок 1. Полезные модули

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -8,28 +8,6 @@ from subprocess import Popen, PIPE
 import chardet as chardet
 
 
-# def host_ping(hosts):
-#     for ip in hosts:
-#         name = ip.exploded if isinstance(ip, IPv4Address) else ip
-#         args = ["ping", name]
-#         process = Popen(args, stdout=PIPE)
-#
-#         data = process.communicate()
-#         result = chardet.detect(data[0])
-#         out = data[0].decode(result['encoding'])
-#         if out.find('Заданный узел недоступен') > 0:
-#             print(name, '- Узел недоступен')
-#         else:
-#             print(name, '- Узел доступен')
-#
-#
-# if __name__ == '__main__':
-#     host_list = [ip_address(f'192.168.0.{x}') for x in range(1, 11)]
-#     host_list.append('yandex.ru')
-#
-#     host_ping(host_list)
-
-
 def host_ping(hosts):
     for ip in hosts:
         name = ip.exploded if isinstance(ip, IPv4Address) else ip
